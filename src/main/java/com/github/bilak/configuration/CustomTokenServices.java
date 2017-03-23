5fa6b726765747a4218e80b8e23776bf95b0f354
bilak <bilaak@gmail.com>
2017-03-23 11:08:05 +0100

    fixed postgre issue with spring-retry. Unfortunately created issue for oracle database.

--- src/main/java/com/github/bilak/configuration/CustomTokenServices.java
+++ src/main/java/com/github/bilak/configuration/CustomTokenServices.java
@@ -3,12 +3,12 @@ package com.github.bilak.configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author lvasek.
@@ -18,16 +18,27 @@ public class CustomTokenServices extends DefaultTokenServices {
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomTokenServices.class);
 
 	@Override
-	@Transactional(isolation = Isolation.DEFAULT)
+	@Retryable(include = DuplicateKeyException.class)
 	public OAuth2AccessToken createAccessToken(OAuth2Authentication authentication) throws AuthenticationException {
 		try {
 			return super.createAccessToken(authentication);
-		} catch (DuplicateKeyException dke) {
-			LOGGER.info(String.format("Duplicate user found for %s", authentication.getUserAuthentication().getPrincipal()));
-			return super.getAccessToken(authentication);
+		} catch (DuplicateKeyException ex) {
+			LOGGER.info(String.format("DuplicateKeyException while creating access token %s", ex));
+			throw ex;
 		} catch (Exception ex) {
 			LOGGER.info(String.format("Exception while creating access token %s", ex));
+			throw new RuntimeException(ex);
 		}
-		return null;
+	}
+
+	@Recover
+	public OAuth2AccessToken recoverAccessToken(OAuth2Authentication authentication)
+			throws AuthenticationException {
+		try {
+			return getAccessToken(authentication);
+		} catch (Exception ex) {
+			LOGGER.error("Unable to recover from createAccessToken error", ex);
+		}
+		throw new IllegalStateException("Cannot create access token");
 	}
 }
---
+++ src/main/java/com/github/bilak/configuration/RetryConfiguration.java
@@ -0,0 +1,12 @@
+package com.github.bilak.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+/**
+ * @author lvasek.
+ */
+@Configuration
+@EnableRetry
+public class RetryConfiguration {
+}
